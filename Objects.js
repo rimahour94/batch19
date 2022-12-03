8bbe37f5dc153e19ccc477860f11b86cd6b7bfe1
RITESH MAHOUR <riteshmahour94@gmail.com>
2022-12-03 11:25:01 +0530

    arrayobjects

--- Objects.js
+++ Objects.js
@@ -1,4 +1,4 @@
-const obj = {
+let obj = {
   name: "Aman",
   cities: ["Noida", "Delhi", "Pune"],
   funA: (a, b) => {
@@ -28,18 +28,18 @@ const obj = {
 
 const { name, cities, funA, innerObj } = obj;
 const { name1, type, class1 } = innerObj;
-console.log(name);
-console.log(cities);
+// console.log(name);
+// console.log(cities);
 
-const v = funA(34, 6);
-console.log(v);
+// const v = funA(34, 6);
+// console.log(v);
 
-console.log(name1);
-console.log(type);
+// console.log(name1);
+// console.log(type);
 
 const { log } = console;
 
-log(class1);
+// log(class1);
 
 // let console = {
 //   log: function (str) {
@@ -47,4 +47,42 @@ log(class1);
 //   },
 // };
 
-console.log("Batch 19 TRISECT ");
+// console.log("Batch 19 TRISECT ");
+
+// let newobj = obj;
+
+// newobj.name = "Khushboo";
+
+// console.log(newobj);
+// console.log(obj);
+
+// spread operator
+
+// let newobj = { ...obj };
+// newobj.name = "Khushboo";
+
+// console.log(newobj);
+// console.log(obj);
+
+// new key add
+obj = { ...obj, company: "GOOGLE", compnayId: "G101" };
+
+// console.log(obj);
+
+// const v1 = Object.values(obj);
+// console.log(v1);
+
+const k1 = Object.keys(obj);
+// console.log(k1);
+
+// array of object
+const arr = [
+  { name: "Kamal", id: 10, city: "Noida", company: "FACEBOOK" },
+  { name: "Raman", id: 20, city: "Gr Noida", company: "Google" },
+  { name: "Naman", id: 30, city: "Pune", company: "Netflix" },
+  { name: "Khushboo", id: 40, city: "Gurugram", company: "Microsoft" },
+  { name: "Neha", id: 50, city: "Hydrabad", company: "Apple" },
+];
+
+console.log(arr[1].company);
+// console.log(arr.name);
